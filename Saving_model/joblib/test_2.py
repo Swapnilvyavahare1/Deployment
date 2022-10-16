71150c68e34f774cd16d7270d190bf69280335cf
Swapnil <swapnil.vyavahare@gmail.com>
2022-10-16 12:32:17 +0530

    Saving the model

---
+++ Saving_model/joblib/test_2.py
@@ -0,0 +1,10 @@
+import joblib
+
+#load the model
+model=joblib.load('diabetes_79.pkl')
+result=model.predict([[1,1,1,1,1,1,1,1]])
+
+if result[0]==1:
+    print("Diabetic")
+else:
+    print("Non-Diabetic")
